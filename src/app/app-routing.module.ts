4f8e469e4288175f5f937ea7434780933c03fb02
Jest <camil.hirane@ynov.com>
2022-04-23 02:15:49 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {MenuComponent} from "./pages/menu/menu.component";
 import {AboutComponent} from "./pages/about/about.component";
 import {ContactComponent} from "./pages/contact/contact.component";
 import {MenupageComponent} from "./pages/menupage/menupage.component";
+import {PrestationComponent} from "./pages/prestation/prestation.component";
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path:'menu/:id',component:MenupageComponent},
   {path:'about',component:AboutComponent},
   {path:'contact',component:ContactComponent},
+  {path:'prestation',component:PrestationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { MenupageComponent } from './pages/menupage/menupage.component';
+import { PrestationComponent } from './pages/prestation/prestation.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { MenupageComponent } from './pages/menupage/menupage.component';
     MenuComponent,
     AboutComponent,
     ContactComponent,
-    MenupageComponent
+    MenupageComponent,
+    PrestationComponent
   ],
   imports: [
     BrowserModule,
